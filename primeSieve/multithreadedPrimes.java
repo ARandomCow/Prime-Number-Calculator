50730ad30cd2324db052a9b2f08acad2f1933049
ARandomCow <107651457+ARandomCow@users.noreply.github.com>
2023-02-01 18:30:46 -0700

    Update multithreadedPrimes.java

--- primeSieve/multithreadedPrimes.java
+++ primeSieve/multithreadedPrimes.java
@@ -5,17 +5,17 @@ import java.io.*;
 public class multithreadedPrimes {
 
     public static void main(String[] args) {
-        System.out.println("Something is working");
+
         final long startTime = System.currentTimeMillis();
 
         Integer primeMax = 1000000;
         int numOfPrimes = 78497;
         // its the number of primes - 1 (because this algorithm doesnt worry about 2 or
         // multiples of it)
-        // primeMax = 100, numOfPrimes = 24
-        // if primeMax = 1,000 then numOfPrimes = 167
-        // if primeMax = 10,000 then numOfPrimes = 1228
-        // if primeMax = 1,000,000, then numOfPrimes = 78,497
+        // if primeMax = 100, numOfPrimes = 24
+        // if 1,000 then 167
+        // if 10,000 then 1228
+        // if 1,000,000, then  78,497
         // if 100,000,000, then 5761454
         // if primeMax = 1,000,000,000 then numOfPrimes = 50847533
         // --------------------------------------------------------------------------------------------------
@@ -37,12 +37,15 @@ public class multithreadedPrimes {
         int add = 1000000;
         int newPrimeLength = 100000;
         int numOfThreads = 5;
+        long startDifference = 200000000;
 
-        multithreadMethods threadi = new multithreadMethods(start, 0, primeArray, newPrimeLength, 1, numOfThreads);
+        multithreadMethods threadi = new multithreadMethods(start, 1, primeArray, 
+        newPrimeLength, 1, numOfThreads, startDifference);
         Thread t1 = new Thread(threadi);
 
-        for (int i = 1; i <= numOfThreads; i++) {
-            threadi = new multithreadMethods(start + (add * i), add, primeArray, newPrimeLength, i, numOfThreads);
+        for (int i = 0; i < numOfThreads; i++) {
+            threadi = new multithreadMethods(start + (startDifference * i), 
+            add, primeArray, newPrimeLength, i, numOfThreads, startDifference);
             t1 = new Thread(threadi);
             t1.start();
         }
